040c0f5354a03d376351a907c54644d3575149ad
PedroSawczuk <pedrosawczuk53@gmail.com>
2024-10-09 20:14:28 -0400

    Style API responses

--- src/components/WheatherInfo/WheatherInfo.jsx
+++ src/components/WheatherInfo/WheatherInfo.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 
 
 export function WeatherInfo({ weather }) {
@@ -6,19 +7,29 @@ export function WeatherInfo({ weather }) {
     return (
         <>
             <h2 className="text-zinc-100 py-2 text-2xl">{weather.name}</h2>
-            
-            <div>
-                <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt="" />
-                <p>{Math.round(weather.main.temp)}</p>
-            </div>
-            <div>
-                <p>{weather.weather[0].description}</p>
-            </div>
 
             <div>
-                <p>Sensação Térmica: {Math.round(weather.main.feels_like)}</p>
-                <p>Umidade: {Math.round(weather.main.humidity)}%</p>
-                <p>Pressão: {Math.round(weather.main.pressure)}</p>
+                <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} className="size-20" alt="" />
+                <p className="text-2xl text-zinc-50">{Math.round(weather.main.temp)}ºC</p>
+            </div>
+            <div className="text-center">
+                <p className="text-zinc-50 py-2 text-xl">Situaçao:</p>
+                <p className="text-zinc-50 text-2xl capitalize">{weather.weather[0].description}</p>
+                <p className="p-2 text-zinc-500">-----------------------------------------------------------------</p>
+            </div>
+
+            <hr />
+
+            <div className="text-center gap-2">
+                <p className="text-zinc-50 py-2 text-xl">Sensação Térmica: </p>
+                <p className="text-zinc-50 text-2xl">{Math.round(weather.main.feels_like)}ºC</p>
+                <p className="p-2 text-zinc-500">-----------------------------------------------------------------</p>
+                <p className="text-zinc-50 py-2 text-xl">Umidade:</p>
+                <p className="text-zinc-50 text-2xl">{Math.round(weather.main.humidity)}%</p>
+                <p className="p-2 text-zinc-500">-----------------------------------------------------------------</p>
+                <p className="text-zinc-50 py-2 text-xl">Pressão:</p>
+                <p className="text-zinc-50 text-2xl">{Math.round(weather.main.pressure)}</p>
+                <p className="p-2 text-zinc-500">-----------------------------------------------------------------</p>
             </div>
 
         </>
